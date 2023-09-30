7cba027a76a65c779a2ba8d64e347687c0fcd914
rvasquez <rodrigovasquez225@gmail.com>
2023-09-29 23:44:09 -0600

    rvasquez-primer commit

---
+++ Models/RedirectToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class RedirectToken
+    {
+        public string clientId { get; set; }
+        public string scope { get; set; }
+        public string state { get; set; }
+        public string redirectUri { get; set; }
+        public string clientSecret { get; set; }
+    }
+}
